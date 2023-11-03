25756500b6dfc9e53c5808553d8f1ab8963a1953
Adrian <adrian@mxlinux.org>
2023-11-02 21:38:34 -0400

    Change cmd to add runAsRoot option and add helper and policy

--- cmd.cpp
+++ cmd.cpp
@@ -1,11 +1,18 @@
 #include "cmd.h"
 
+#include <QApplication>
 #include <QDebug>
 #include <QEventLoop>
+#include <QFileInfo>
 
 Cmd::Cmd(QObject *parent)
-    : QProcess(parent)
+    : QProcess(parent),
+      elevate {"pkexec"},
+      helper {"/usr/lib/" + QApplication::applicationName() + "/helper"}
 {
+    if (!QFileInfo::exists("/usr/bin/pkexec")) {
+        elevate = "su-to-root -X -c";
+    }
     connect(this, &Cmd::readyReadStandardOutput, [this] { emit outputAvailable(readAllStandardOutput()); });
     connect(this, &Cmd::readyReadStandardError, [this] { emit errorAvailable(readAllStandardError()); });
     connect(this, &Cmd::outputAvailable, [this](const QString &out) { out_buffer += out; });
@@ -18,11 +25,26 @@ bool Cmd::run(const QString &cmd, bool quiet)
     return run(cmd, &output, quiet);
 }
 
+bool Cmd::runAsRoot(const QString &cmd, bool quiet)
+{
+    return run(elevate + " " + helper + " " + cmd, quiet);
+}
+
+bool Cmd::runAsRoot(const QString &cmd, QString *output, bool quiet)
+{
+    return run(elevate + " " + helper + " " + cmd, output, quiet);
+}
+
 QString Cmd::getCmdOut(const QString &cmd, bool quiet)
 {
     QString output;
     run(cmd, &output, quiet);
     return output;
+}
+
+QString Cmd::getCmdOutAsRoot(const QString &cmd, bool quiet)
+{
+    return getCmdOut(elevate + " " + helper + " " + cmd, quiet);
 }
 
 bool Cmd::run(const QString &cmd, QString *output, bool quiet)
@@ -33,8 +55,9 @@ bool Cmd::run(const QString &cmd, QString *output, bool quiet)
         qDebug() << "Process already running:" << this->program() << this->arguments();
         return false;
     }
-    if (!quiet)
+    if (!quiet) {
         qDebug().noquote() << cmd;
+    }
     QEventLoop loop;
     connect(this, &Cmd::finished, &loop, &QEventLoop::quit);
     start(QStringLiteral("/bin/bash"), {QStringLiteral("-c"), cmd});
--- cmd.h
+++ cmd.h
@@ -10,17 +10,22 @@ class Cmd : public QProcess
 {
     Q_OBJECT
 public:
-    explicit Cmd(QObject *parent = nullptr);
-    bool run(const QString &cmd, bool quiet = false);
-    bool run(const QString &cmd, QString *output, bool quiet = false);
     [[nodiscard]] QString getCmdOut(const QString &cmd, bool quiet = false);
+    [[nodiscard]] QString getCmdOutAsRoot(const QString &cmd, bool quiet = false);
+    bool run(const QString &cmd, QString *output, bool quiet = false);
+    bool run(const QString &cmd, bool quiet = false);
+    bool runAsRoot(const QString &cmd, QString *output, bool quiet = false);
+    bool runAsRoot(const QString &cmd, bool quiet = false);
+    explicit Cmd(QObject *parent = nullptr);
 
 signals:
-    void finished();
     void errorAvailable(const QString &err);
+    void finished();
     void outputAvailable(const QString &out);
 
 private:
+    QString elevate;
+    QString helper;
     QString out_buffer;
 };
 
